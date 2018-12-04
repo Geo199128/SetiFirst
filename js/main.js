20db1f09046b90855157a7a59f0946194efdd5cd
George Matta <George.Matta@linkdev.com>
2018-12-04 20:10:57 +0200

    creating home explore and home about sections

--- js/main.js
+++ js/main.js
@@ -3,19 +3,33 @@ var InitCarousels = function () {
     var heroCarousel = $('.hero-carousel');
     if (heroCarousel.length) {
         heroCarousel.owlCarousel({
-            items:1,
-            loop:true,
+            items: 1,
+            loop: true,
+            /*stagePadding: 0,
+            singleItem:true,
+            margin:0,*/
             //autoplay:true,
-            autoplayTimeout:5000,
-            nav:true,
-            navText:['<i class="icon-right-arrow"></i>','<i class="icon-right-arrow"></i>'],
-            dots:true,
-            mouseDrag:true,
-            touchDrag:true,
+            autoplayTimeout: 5000,
+            nav: true,
+            navText: ['<i class="icon-right-arrow"></i>', '<i class="icon-right-arrow"></i>'],
+            dots: true,
+            mouseDrag: true,
+            touchDrag: true,
         });
     }
 }
 
+//Init parallax
+var InitParallax = function () {
+    //$('.parallaxie').parallaxie();
+    $('.parallaxie').miniParallax({
+        inset: 1,
+        speed: 0.15
+    });
+
+}
+
 $(document).ready(function () {
     InitCarousels();
+    //InitParallax();
 });
